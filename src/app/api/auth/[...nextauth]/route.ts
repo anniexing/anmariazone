a4f3788b7d61b7c743b260397e45c7c131b258f8
Annie Xing <mxxing@live.com>
2023-12-07 14:51:08 +0100

    add nextjs auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,13 @@
+import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/db/prisma";
+import { Adapter } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+    }),
+  ],
+};
